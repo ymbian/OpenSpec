1bdc0b7ff8a3715986d6c9391965ba49ce39320f
rose <1206767881@qq.com>
2026-03-12 15:38:20 +0800

    InfraSpec Dashboard

--- src/core/view.ts
+++ src/core/view.ts
@@ -13,7 +13,7 @@ export class ViewCommand {
       process.exit(1);
     }
 
-    console.log(chalk.bold('\nOpenSpec Dashboard\n'));
+    console.log(chalk.bold('\nInfraSpec Dashboard\n'));
     console.log('═'.repeat(60));
 
     // Get changes and specs data
